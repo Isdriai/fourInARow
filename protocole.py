ece4e952ff876004f1290b919cabb96d39e148bd
PicotRodolphe <picot.rodolphe@gmail.com>
2017-12-03 19:54:04 +0100

    commit

--- protocole.py
+++ protocole.py
@@ -1,43 +1,64 @@
 class Protocole(object):
 
-	version="1"
+	VERSION="1"
+	UNKNOW_LOGIN = "UNKNOW_LOGIN"
+	CREATE_ACCOUNT = "CREATE_ACCOUNT: "
+	CONFIRM_ACCOUNT = "CONFIRM_ACCOUNT: "
+	PASSWORD = "PASSWORD:"
+	ASK_PASSWORD = "ASK_PASSWORD"
+	ERROR_PASSWORD = "WRONG PASSWORD"
+	CREATE_ROOM = "CREATE_ROOM"
+	SELECT_ROOM = "SELECT_ROOM: "
+	ROOM_CONFIRM = "ROOM_CONFIRM"
+	ERROR_ROOM = "ROOM does not exist"
+	COLOR = "COLOR: "
+	ERROR_COLOR = "Color not available"
+	BEGIN = "BEGIN"
+	ERROR_MOVE = "Impossible move"
+	QUIT_ROOM = "QUIT_ROOM"
+	QUIT_SERVER = "QUIT_SERVER"
+	GAME_FINISHED = "GAME_FINISHED"
+	ASK_LIST = "ASK_LIST"
+	LIST = "LIST: "
+	QUERY_LOGIN = "QUERY_LOGIN"
+	LOGIN_INFO = "LOGIN_INFO"
+
 
 	def sendMove():
 
-		
+
 	def receiveMove():
 
-		
+
 	def begin():
 
-		
+
 	def receiveBoard():
 
-		
+
 	def sendVersion():
 
-		
+
 	def sendLogin():
 
-		
+
 	def receiveAnswerLogin():
 
-		
+
 	def sendPassword():
 
-		
+
 	def receiveRooms():
 
-		
+
 	def createRoom():
 
-		
+
 	def joinRoom():
 
-		
+
 	def receiveAnswerRoom():
 
-		
+
 	def initColor():
 
-		
